0e74a8f150c3e5b96c4d01b641c50c7d00d41a13
ramotswa <ramotswa@gmail.com>
2015-02-20 13:25:58 +0000

    Create osgb-wmts.js

---
+++ leaflet/osgb-wmts.js
@@ -0,0 +1,66 @@
+// Define a Proj4Leaflet crs instance configured for British National Grid
+// (EPSG:27700) and the resolutions of our base map
+var crs = new L.Proj.CRS(
+    'EPSG:27700',
+    '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs',
+    {
+        resolutions: [2500,1000,500,200,100,50,25,10,5,4,2.5,2,1.75,1,0.75,0.5,0.25]
+    }
+);
+
+// Define a standard Leaflet map specifying our crs instance and define a WMS
+// base map
+var map = new L.Map('map', {
+    crs: crs   
+});
+
+var layerTimerStart;
+var layerTimerStop;
+var tileTimerStart;
+var tileTimerStop;
+var averageTileLoadTime = 0;
+var tileCount;
+
+map.setView([51.5, -0.2], 10);
+
+
+var myLayer = L.tileLayer.wms('URLINHERE:8080/geoserver/gwc/service/wms?',
+ {
+    //subdomains: ['1','2'],
+    layers: 'collinsbartholomew:Londonpanorama20',
+    tiled: true,
+    format: 'image/png8',
+    transparent: true,
+    
+    tileSize: 128,
+    maxZoom: 14,
+    minZoom: 0,
+    continuousWorld: true
+}).addTo(map);
+
+myLayer.on("loading",function() {
+    layerTimeStart = new Date();
+});
+
+myLayer.on("load",function() { 
+    layerTimeStop = new Date();
+    var timeDiff = Math.abs(layerTimeStop.getTime() - layerTimeStart.getTime());
+    console.log("all visible tiles have been loaded in " + timeDiff + ' milliseconds');
+    console.log("average tile load time is " + averageTileLoadTime + ' milliseconds for ' +  tileCount + ' tiles.') 
+    tileCount = 0;
+    averageTileLoadTime = 0;
+});
+
+myLayer.on("tileloadstart",function() {
+    tileTimerStart = new Date();
+});
+
+myLayer.on("tileload",function() { 
+    tileCount ++;
+    tileTimerStop = new Date();
+    var timeDiff = Math.abs(tileTimerStop.getTime() - tileTimerStart.getTime());
+    averageTileLoadTime =+ timeDiff;
+    averageTileLoadTime = averageTileLoadTime / tileCount;
+    
+    tileTimerStart = new Date();
+});
